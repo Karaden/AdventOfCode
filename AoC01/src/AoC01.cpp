2a4dc3ee7f06e86b2711296a06e1f957943bb527
Kate Twiss <karaden@gmail.com>
2017-12-13 20:04:33 +0100

    Refactoring for nicer testing.

--- AoC01/src/AoC01.cpp
+++ AoC01/src/AoC01.cpp
@@ -55,33 +55,20 @@ int puzzleInput[] = {3,1,8,1,3,1,7,4,3,4,9,2,3,5,9,7,2,1,5,9,8,1,1,8,6,9,7,5,5,1
 		5,5,2,2,7,9,8,4,9,9,1,7,3,4,2,4,7,4,7,9,2,9,8,4,4,2,5,6,2,9,2,4,8,4,9,2,8,4,7,8,2,7,6,5,3,1,3,3,5,8,3,2,1,5,5,3,9,3,2,5,8,
 		6,6,8,8,1,6,6,2,1,5,9,4,2,1,9,8,7,3,1,5,1,8,6,9,1,4,7,6,9,4,7,8,9,4,7,3,8,9,1,8,8,3,8,2,3,8,3,5,4,6,8,8,1,6,2,2,2,4,6,7,9,
 		3,7,8,1,8,4,6,2,5,4,2,5,3,7,5,9,7,1,4,5,7,3,3,5,4,5,4,4,9,9,7,8,5,3,1,5,3,7,9,8,8,6,2,4,3,6,8,8,7,8,8,9,3,1,8,6,4,6,6,4,3,
-		3,5,9,5,5,5,6,6,3,1,3,5,4,7,6,2,6,1,8,6,3}
-;
+		3,5,9,5,5,5,6,6,3,1,3,5,4,7,6,2,6,1,8,6,3};
 
-int calculate(int* data, int arrayLength);
-
-int main()
+void test(int actual, int expected)
 {
-
-	int result = calculate(sample4, (sizeof(sample4)/sizeof(*sample4)));
-
-	if (result != solution4)
-	{
-		cout << "Wrong result: " << result << endl;
-	}
-	else
-	{
-		cout << "Looks good. Result is: " << result << endl;
-	}
-
-
-	result = calculate(puzzleInput, (sizeof(puzzleInput)/sizeof(*puzzleInput)));
-	cout << "Final result is: " << result << endl;
-
-	return 0;
+	if (actual != expected)
+		{
+			cout << "Wrong result: " << actual << endl;
+		}
+		else
+		{
+			cout << "Looks good. Result is: " << actual << endl;
+		}
 
 }
-
 
 int calculate(int* data, int arrayLength)
 {
@@ -109,5 +96,30 @@ int calculate(int* data, int arrayLength)
 
 	return total;
 }
+
+int main()
+{
+
+	int result = calculate(sample1, (sizeof(sample1)/sizeof(*sample1)));
+	test(result, solution1);
+
+	result = calculate(sample2, (sizeof(sample2)/sizeof(*sample2)));
+	test(result, solution2);
+
+	result = calculate(sample3, (sizeof(sample3)/sizeof(*sample3)));
+	test(result, solution3);
+
+	result = calculate(sample4, (sizeof(sample4)/sizeof(*sample4)));
+	test(result, solution4);
+
+	result = calculate(puzzleInput, (sizeof(puzzleInput)/sizeof(*puzzleInput)));
+	cout << "Final result is: " << result << endl;
+
+	return 0;
+
+}
+
+
+
 
 
