3999e9a221f01dae079b82f69e5fb359adbd2065
Kate Twiss <karaden@gmail.com>
2018-01-04 12:57:56 +0100

    Initial commit (w/ correct sample and input data)

---
+++ AoC02-B/src/AoC02-B.cpp
@@ -0,0 +1,44 @@
+//============================================================================
+// Name        : AoC02-B.cpp
+// Author      : KateT
+// Version     :
+// Copyright   : Your copyright notice
+// Description : the goal is to find the only two numbers in each row where
+//				 one evenly divides the other - that is, where the result
+//				 of the division operation is a whole number. They would
+//				 like you to find those numbers on each line, divide them,
+//				 and add up each line's result.
+//
+//
+//    In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+//    In the second row, the two numbers are 9 and 3; the result is 3.
+//    In the third row, the result is 2.
+//
+//In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+vector<vector<int> > data;
+vector<vector<int> > sampleData;
+int sampleSolution = 9;
+
+
+
+
+
+
+
+int main() {
+	cout << "AoC02-B:" << endl; // prints AoC02-B:
+	return 0;
+}
