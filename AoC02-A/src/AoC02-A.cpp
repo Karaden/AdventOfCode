5b8af4e1dbcc8496860ce59974cf46eb5951a093
Kate Twiss <karaden@gmail.com>
2018-01-04 12:56:47 +0100

    Added nice file descriptor

--- AoC02-A/src/AoC02-A.cpp
+++ AoC02-A/src/AoC02-A.cpp
@@ -3,7 +3,15 @@
 // Author      : KateT
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : calculate the spreadsheet's checksum. For each row, determine
+//				 the difference between the largest value and the smallest
+//				 value; the checksum is the sum of all of these differences.
+//
+//    The first row's largest and smallest values are 9 and 1, and their difference is 8.
+//    The second row's largest and smallest values are 7 and 3, and their difference is 4.
+//    The third row's difference is 6.
+//
+//	In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 //============================================================================
 
 #include <iostream>
